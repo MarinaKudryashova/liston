5e62968e9386bca31df9a2849502952ba1415dc3
SvetaVarghese <svetlanavarghese@gmail.com>
2026-06-23 13:21:16 +0300

    add sec-catalog, sec-popular, card-category, fix lang

--- src/js/components/lang.js
+++ src/js/components/lang.js
@@ -6,8 +6,18 @@ btn.addEventListener('click', () => {
   const isOpen = lang.classList.toggle('is-open');
 
   if(isOpen) {
-    list.style.maxHeight = `${list.scrollHeight + 12}px`
+    list.style.maxHeight = `${list.scrollHeight}px`
   } else {
     list.style.maxHeight = '0px';
   }
 })
+
+// клик вне компонента
+document.addEventListener('click', (e) => {
+  const isClickInside = lang.contains(e.target);
+
+  if (!isClickInside && lang.classList.contains('is-open')) {
+    lang.classList.remove('is-open');
+    list.style.maxHeight = '0px';
+  }
+});
